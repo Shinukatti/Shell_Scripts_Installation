51df4886a448b8a6665230f07a3a644a8e85a910
Shinukatti <126487719+Shinukatti@users.noreply.github.com>
2023-04-16 19:46:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-sudo -i
-
 sudo apt update -y
 
 sudo apt install default-jre -y
 
-java -version
+sudo java -version
 
 sudo apt update -y
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+ sudo /usr/share/keyrings/jenkins-keyring.asc > /dev/null
   
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+ sudo /etc/apt/sources.list.d/jenkins.list > /dev/null
   
 sudo apt-get update
 sudo apt-get install jenkins
